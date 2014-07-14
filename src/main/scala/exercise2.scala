afc082384b0dcfda1156a7b1b8c9bd3cc136687f
Syth Ryan <syth.ryan@banno.com>
2014-07-14 16:16:12 -0500

    exercise 2

---
+++ src/main/scala/exercise2.scala
@@ -0,0 +1,19 @@
+object exercise2 {
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    def innerIsSorted(anArray: Array[A]): Boolean = anArray.length match {
+      case 1 => true
+      case _ => if (gt(anArray(0), anArray(1))) innerIsSorted(anArray.tail) else false
+    }
+    innerIsSorted(as)
+  }
+
+  def ascending(x: Int, y: Int): Boolean = {
+    (x < y)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("is Array(1, 2, 3) in ascending order: " + isSorted(Array(1,2,3), ascending))
+    println("is Array(3, 2, 1) in ascending order: " + isSorted(Array(3,2,1), ascending))
+  }
+  
+}
--- src/main/scala/listing2point1.scala
+++ src/main/scala/listing2point1.scala
@@ -7,12 +7,22 @@ object MyModule {
     if(n < 0 ) -n
       else n
 
-  private def formatAbs(x: Int) = {
-    val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
+  def factorial(n: Int): Int = {
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n-1, n*acc)
+
+    go(n, 1)
   }
 
-  def main(args: Array[String]): Unit =
-    println(formatAbs(-42))
+  def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d."
+    msg.format(name, n, f(n))
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(formatResult("The absolute value", -42, abs))
+    println(formatResult("The factorial", 7, factorial))
+  }
 }
   
